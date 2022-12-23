154a26d93dcdf72ad3bf10a6f9a90476e90d018b
veer-p <veerp@student.ubc.ca>
2022-12-23 11:45:22 -0800

    add youtube to mp3 retro script

---
+++ youtube_to_mp3_retro.py
@@ -0,0 +1,13 @@
+import pytube
+import os
+
+url = input("enter the url: ")
+
+path= "/Users/veerbhatia/Downloads"
+
+audio = pytube.YouTube(url).streams.get_audio_only()
+audio_download = audio.download(output_path=path)
+base, ext = os.path.splitext(audio_download)
+new_file = base + '.mp3'
+os.rename(audio_download, new_file)
+
